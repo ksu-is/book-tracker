ae1da4143d64a2fe0a3663127067d01a62cef395
Diana Rosales <drosale9@students.kennesaw.edu>
2024-11-17 23:23:40 -0500

    Fixed the code to be more neat.
    Added 'break' in the while loop and fixed a couple more things

--- main.py
+++ main.py
@@ -17,30 +17,32 @@ def category():
 
 # function to add a book to chosen category    
 def add_books():
-        chosen_category = input("Enter your chosen category: ")
-        book = input("Enter book title: ")
+        while True:
+            chosen_category = input("Enter your chosen category: ")
+            book = input("Enter book title and author: ")
 
-        if chosen_category == "Want to Read":
-            to_read.append(book)
-            print(book,"added to 'Want to Read")
-        elif chosen_category == "Currently Readinig":
-            currently_reading.append(book)
-            print(book,"added to 'Currently Reading'")
-        elif chosen_category == "Read":
-            read.append(book)
-            print(book,"added to 'Read'")
-        elif chosen_category == "Other":
-            other.append(book)
-            print(book,"added to 'Other'")
-        else:
-            print("Category does not exist. Try another.")
+            if chosen_category == "Want to Read":
+                to_read.append(book)
+                print(book,"added to 'Want to Read")
+            elif chosen_category == "Currently Reading":
+                currently_reading.append(book)
+                print(book,"added to 'Currently Reading'")
+            elif chosen_category == "Read":
+                read.append(book)
+                print(book,"added to 'Read'")
+            elif chosen_category == "Other":
+                other.append(book)
+                print(book,"added to 'Other'")
+            else:
+                if chosen_category and book == 'q':
+                     break
 
 # function to view books that have been tracked
 def view_books():
      print("\nBooks In Your Library")
-     print(to_read)
-     print(currently_reading)
-     print(read)
+     print("Want to Read:",to_read)
+     print("Currently Reading:",currently_reading)
+     print("Read:",read)
 
 category()
 add_books()
