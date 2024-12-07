c168f7db1ae3373f3d24908f1da6fb4999af0f8f
Diana Rosales <drosale9@students.kennesaw.edu>
2024-12-06 23:51:07 -0500

    Added a new function
    Function calls other functions on a loop

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def category():
         print("1.Want to Read")
         print("2.Currently Reading")
         print("3.Read")
-        print("4.Other")
-        return input("Choose a category (1-4): ")
+        print("4.View Library")
+        return input 
 
 #function to add a book to chosen category   
 def add_books():
@@ -26,6 +26,8 @@ def add_books():
             if chosen_category == "1":
                 to_read.append(book)
                 print(book,"added to 'Want to Read")
+                
+                break
             elif chosen_category == "2":
                 currently_reading.append(book)
                 print(book,"added to 'Currently Reading'")
@@ -37,6 +39,7 @@ def add_books():
             elif chosen_category == "3":
                 read.append(book)
                 print(book,"added to 'Read'")
+                break
             elif chosen_category == "4":
                 other.append(book)
                 print(book,"added to 'Other'")
@@ -53,8 +56,8 @@ def valid_date(months,day_in_month):
 
 #start date function
 def start_date():
-    start_month = input("Month: ")
-    start_day = input("Day: ")
+    start_month = input("Start Month: ")
+    start_day = input("Start Day: ")
     if start_month in months:
         months_index = start_month.index(start_month)
         amount_of_days = day_in_month[months_index]
@@ -63,26 +66,30 @@ def start_date():
 
 #end date function
 def end_date():
-    end_month = input("Month: ")
-    end_day = input("Day: ")
+    end_month = input("End Month: ")
+    end_day = input("End Day: ")
     if end_month in months:
         months_index = end_month.index(end_month)
         amount_of_days = day_in_month[months_index]
         return 1 <= int(end_day) <= amount_of_days
     return False
-#print("Enter end date: ")
-#while True:
-     #end_month = input("Month: ")
-     #end_day = input("Day: ")
-     #if chosen_date(end_month, end_day):
-          #break
 
 #function to view books that have been tracked
-#def view_books():
-     #print("\nBooks In Your Library")
-     #print("Want to Read:",to_read)
-     #print("Currently Reading:",currently_reading)
-     #print("Read:",read)
+def view_books():
+     print("\nBooks In Your Library")
+     print("Want to Read:",to_read)
+     print("Currently Reading:",currently_reading)
+     print("Read:",read)
+
+#function to call functions repeatedly
+def call_functions():
+    while True:
+        category()
+        add_books()
+        start_date()
+        end_date()
+        valid_date(months,day_in_month)
+        view_books()
 
 # storing end dates and start dates
 #completed_books.append(start_month)
@@ -90,14 +97,16 @@ def end_date():
 #completed_books.append(end_month)
 #completed_books.append(end_day)
 
-# calling the functions
-category()
-add_books()
-start_date()
-end_date()
-valid_date(months,day_in_month)
-#view_books()
+call_functions()
 
+
+# calling the functions
+#category()
+#add_books()
+#start_date()
+#end_date()
+#valid_date(months,day_in_month)
+#view_books()
 
 
 
