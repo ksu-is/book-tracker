6d237dc932f2bf9dc4ba469cdc619601fc6694e2
Diana Rosales <drosale9@students.kennesaw.edu>
2024-12-07 11:36:21 -0500

    Organized and fixed a couple functions

--- main.py
+++ main.py
@@ -2,9 +2,6 @@
 to_read = []
 currently_reading = []
 read = []
-other = []
-dates = []
-completed_books = []
 
 # function to choose a category
 def category():
@@ -15,98 +12,92 @@ def category():
         print("2.Currently Reading")
         print("3.Read")
         print("4.View Library")
-        return input 
+        print("5.Quit")
+        decision = input("Choose a category(1-5): ")
+        if decision in ["1","2","3","5"]:
+            return decision
+        elif decision in ["4"]:
+            print(view_books)
+        else: 
+            print("Invalid choice. Try again.")
 
 #function to add a book to chosen category   
 def add_books():
         while True:
             chosen_category = input("Enter your chosen category: ")
+            if chosen_category == "5":
+                return False
             book = input("Enter book title and author: ")
-
             if chosen_category == "1":
                 to_read.append(book)
-                print(book,"added to 'Want to Read")
-                
+                print(book," added to 'Want to Read")
                 break
             elif chosen_category == "2":
-                currently_reading.append(book)
-                print(book,"added to 'Currently Reading'")
-                while True:
-                    start_month = input("Start Month: ")
-                    start_day = input("Start Day: ") 
-                    if valid_date(start_month,start_day):
-                        break
+                start = start_date()
+                print(book + " added to 'Currently Reading', started on " + start)
+                currently_reading.append((book,start))
+                    
             elif chosen_category == "3":
-                read.append(book)
-                print(book,"added to 'Read'")
+                start = start_date()
+                end = end_date()
+                read.append((book,start,end))
+                print(book + " added to 'Read',read from", start + "to" + end)
                 break
             elif chosen_category == "4":
-                other.append(book)
-                print(book,"added to 'Other'")
-            elif chosen_category == 'q':
-                if book == 'q':
-                    break
+                print(view_books)
 
 #function to confirm date
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August','September','October','November','December']
 day_in_month = [31,29,31,30,31,30,31,31,30,31,30,31]
-def valid_date(months,day_in_month):
+def valid_date(month,day):
     months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August','September','October','November','December']
     day_in_month = [31,29,31,30,31,30,31,31,30,31,30,31]
+    if month in months:
+        month_index = months.index(month)
+        return 1 <= int(day) <= day_in_month[month_index]
 
 #start date function
 def start_date():
-    start_month = input("Start Month: ")
-    start_day = input("Start Day: ")
-    if start_month in months:
-        months_index = start_month.index(start_month)
-        amount_of_days = day_in_month[months_index]
-        return 1 <= int(start_day) <= amount_of_days
-    return False
+    while True:
+        start_month = input("Start Month: ")
+        start_day = input("Start Day: ")
+        if valid_date(start_month, start_day):
+            return start_month  + " " + start_day
+        else:
+            print("Invalid date. Try again.")
 
 #end date function
 def end_date():
-    end_month = input("End Month: ")
-    end_day = input("End Day: ")
-    if end_month in months:
-        months_index = end_month.index(end_month)
-        amount_of_days = day_in_month[months_index]
-        return 1 <= int(end_day) <= amount_of_days
-    return False
+    while True:
+        end_month = input("End Month: ")
+        end_day = input("End Day: ")
+        if valid_date(end_month, end_day):
+            return (end_month  + " " + end_day)
+        else:
+            print("Invalid date. Try again.")
 
 #function to view books that have been tracked
 def view_books():
      print("\nBooks In Your Library")
      print("Want to Read:",to_read)
-     print("Currently Reading:",currently_reading)
-     print("Read:",read)
+     print("Currently Reading:",[book[0] for book in currently_reading])
+     print("Read:",[book[0] for book in read])
+     print("\nTracked Dates for Books:")
+     for book in currently_reading:
+        print(book[0]+': Started on',book[1])
+     for book in read:
+        print(book[0]+': Started on',book[1],',Finished on',book[2])
 
 #function to call functions repeatedly
 def call_functions():
     while True:
         category()
         add_books()
-        start_date()
-        end_date()
-        valid_date(months,day_in_month)
         view_books()
-
-# storing end dates and start dates
-#completed_books.append(start_month)
-#completed_books.append(start_day)
-#completed_books.append(end_month)
-#completed_books.append(end_day)
 
 call_functions()
 
 
-# calling the functions
-#category()
-#add_books()
-#start_date()
-#end_date()
-#valid_date(months,day_in_month)
-#view_books()
 
 
 
